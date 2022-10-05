0958bdcf97586394cdaf793d97fe593ed4c08f04
axelsvenn <mrr.axelsven@gmail.com>
2022-10-05 18:48:16 +0300

    ClassWork

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+echo "hello\nworld\n";
+$x = 1;
+?>
+<pre>
+    <?php
+echo $x;
+var_dump($x);
+$x = [2,3,4,5,6,7,8];
+var_dump($x);
+
+for ($i = 0; $i < $_REQUEST['a']; $i++) {
+    echo $i . "<br/>";
+}
+?>
