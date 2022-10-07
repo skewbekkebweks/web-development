e68db7727f683a575a749b69499184645d01aa61
Samsung_1 <mihail_glazov2015@yandex.ru>
2022-10-07 16:59:49 +0300

    multiplication table

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
-<?php
-declare(strict_types = 1);
-echo "hello\nworld\n";
-$x = 1;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tables</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link rel="icon"
+          href="https://sun9-71.userapi.com/impf/c854528/v854528816/113120/rqhbUgxbvmA.jpg?size=986x1080&quality=96&sign=fb15b8926a6494c08c90e142ce2d0996&type=album"/>
+</head>
 <pre>
     <?php
-echo $x;
-var_dump($x);
-$x = [2,3,4,5,6,7,8];
-var_dump($x);
-
-for ($i = 0; $i < $_REQUEST['a']; $i++) {
-    echo $i . "<br/>";
-}
-?>
+    echo "<table>";
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= 10; $j++) {
+            echo "<td>" . $i * $j . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    ?>
